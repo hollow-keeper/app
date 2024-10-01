84d4996b6a53bc43871da987a0f4079373fc2a22
volume02 <borsukboris@gmail.com>
2024-10-01 20:19:00 +0700

    many equipments to one item

--- src/equipment/entities/equipment.entity.ts
+++ src/equipment/entities/equipment.entity.ts
@@ -3,7 +3,7 @@ import {
   Column,
   Entity,
   JoinColumn,
-  OneToOne,
+  ManyToOne,
   PrimaryGeneratedColumn,
 } from 'typeorm';
 
@@ -12,70 +12,70 @@ export class Equipment {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @OneToOne(() => Item, { cascade: true, nullable: true })
+  @ManyToOne(() => Item, { nullable: true })
   @JoinColumn({
     name: 'helmet_id',
     referencedColumnName: 'id',
   })
   helmet?: Item;
 
-  @OneToOne(() => Item, { cascade: true, nullable: true })
+  @ManyToOne(() => Item, { nullable: true })
   @JoinColumn({
     name: 'armor_id',
     referencedColumnName: 'id',
   })
   armor?: Item;
 
-  @OneToOne(() => Item, { cascade: true, nullable: true })
+  @ManyToOne(() => Item, { nullable: true })
   @JoinColumn({
     name: 'arms_id',
     referencedColumnName: 'id',
   })
   arms?: Item;
 
-  @OneToOne(() => Item, { cascade: true, nullable: true })
+  @ManyToOne(() => Item, { nullable: true })
   @JoinColumn({
     name: 'legs_id',
     referencedColumnName: 'id',
   })
   legs?: Item;
 
-  @OneToOne(() => Item, { cascade: true, nullable: true })
+  @ManyToOne(() => Item, { nullable: true })
   @JoinColumn({
     name: 'ring1_id',
     referencedColumnName: 'id',
   })
   ring1?: Item;
 
-  @OneToOne(() => Item, { cascade: true, nullable: true })
+  @ManyToOne(() => Item, { nullable: true })
   @JoinColumn({
     name: 'ring2_id',
     referencedColumnName: 'id',
   })
   ring2?: Item;
 
-  @OneToOne(() => Item, { cascade: true, nullable: true })
+  @ManyToOne(() => Item, { nullable: true })
   @JoinColumn({
     name: 'left_weapon_primary_id',
     referencedColumnName: 'id',
   })
   left_weapon_primary?: Item;
 
-  @OneToOne(() => Item, { cascade: true, nullable: true })
+  @ManyToOne(() => Item, { nullable: true })
   @JoinColumn({
     name: 'right_weapon_primary_id',
     referencedColumnName: 'id',
   })
   right_weapon_primary?: Item;
 
-  @OneToOne(() => Item, { cascade: true, nullable: true })
+  @ManyToOne(() => Item, { nullable: true })
   @JoinColumn({
     name: 'left_weapon_secondary_id',
     referencedColumnName: 'id',
   })
   left_weapon_secondary?: Item;
 
-  @OneToOne(() => Item, { cascade: true, nullable: true })
+  @ManyToOne(() => Item, { nullable: true })
   @JoinColumn({
     name: 'right_weapon_secondary_id',
     referencedColumnName: 'id',
