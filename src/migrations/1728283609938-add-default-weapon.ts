c87797c0ea4fc3897b16dbad011de0cceca40698
n.dremov <pmi22dremov@gmail.com>
2024-10-07 10:54:46 +0400

    add migration with adding noj

---
+++ src/migrations/1728283609938-add-default-weapon.ts
@@ -0,0 +1,13 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AddDefaultWeapon1728283609938 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`INSERT INTO items (name, weight, balance, characteristics_bonus, properties_bonus, type) VALUES ('noj', 2, 3, '{  "strength": 1,  "dexterity": 3}', '{  "armor": 2,  "balance": 4}', 'item')`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DELETE FROM items WHERE name='noj'`);
+    }
+
+}
